7133360db91666d74acde978db74a39e20549c1f
Takashi Matsuoka <matsujirushi@live.jp>
2019-11-02 21:22:25 +0900

    Add section 2 of handson3

--- handson3/sketch/baro-awsiot/baro-awsiot.ino
+++ handson3/sketch/baro-awsiot/baro-awsiot.ino
@@ -1,43 +1,65 @@
+#include <PubSubClient.h>     // https://github.com/knolleary/pubsubclient
+#include <WioLTEClient.h>
 #include <GroveDriverPack.h>  // https://github.com/SeeedJP/GroveDriverPack
 
 #define APN       "soracom.io"
 #define USERNAME  "sora"
 #define PASSWORD  "sora"
 
-#define INTERVAL        (60000)
-#define RECEIVE_TIMEOUT (10000)
+#define MQTT_SERVER_HOST  "beam.soracom.io"
+#define MQTT_SERVER_PORT  (1883)
+
+// Designing MQTT Topics for AWS IoT Core
+#define APPLICATION     "handson3"
+#define CONTEXT         "techplayshibuya"
+#define THING           "BaroDevice1"
+#define TELEMETRY_TOPIC "dt/" APPLICATION "/" CONTEXT "/" THING
+
+#define INTERVAL  (5000)
 
 WioCellular Wio;
+
+WioLTEClient WioClient(&Wio);
+PubSubClient MqttClient;
 
 GroveBoard Board;
 OmronBaro2SMPB02E Sensor(&Board.I2C);
 
 void setup() {
   delay(200);
-
+  
   SerialUSB.begin(115200);
   SerialUSB.println("");
   SerialUSB.println("--- START ---------------------------------------------------");
-
+  
   SerialUSB.println("### I/O Initialize.");
   Wio.Init();
-
+  
   SerialUSB.println("### Power supply ON.");
   Wio.PowerSupplyCellular(true);
   Wio.PowerSupplyGrove(true);
   delay(500);
-
+  
   SerialUSB.println("### Turn on or reset.");
   if (!Wio.TurnOnOrReset()) {
     SerialUSB.println("### ERROR! ###");
     abort();
   }
-
+  
   SerialUSB.println("### Connecting to \"" APN "\".");
   if (!Wio.Activate(APN, USERNAME, PASSWORD)) {
+    SerialUSB.println("### ERROR! ###");
     abort();
   }
-
+  
+  SerialUSB.println("### Connecting to MQTT server \"" MQTT_SERVER_HOST "\"");
+  MqttClient.setServer(MQTT_SERVER_HOST, MQTT_SERVER_PORT);
+  MqttClient.setClient(WioClient);
+  if (!MqttClient.connect(THING)) {
+    SerialUSB.println("### ERROR! ###");
+    abort();
+  }
+  
   SerialUSB.println("### Sensor Initialize.");
   Board.I2C.Enable();
   Sensor.Init();
@@ -55,49 +77,13 @@ void loop() {
   SerialUSB.print(Sensor.Pressure);
   SerialUSB.println("Pa");
   sprintf(data, "{\"temp\":%.1f,\"pres\":%.1f}", Sensor.Temperature, Sensor.Pressure);
-  if (isnan(Sensor.Temperature) || isnan(Sensor.Pressure)) goto err;
-
-  SerialUSB.println("### Open.");
-  int connectId;
-  connectId = Wio.SocketOpen("harvest.soracom.io", 8514, WIO_TCP);
-  if (connectId < 0) {
-    SerialUSB.println("### ERROR! ###");
-    goto err;
+  
+  if (!isnan(Sensor.Temperature) && !isnan(Sensor.Pressure)) {
+    MqttClient.publish(TELEMETRY_TOPIC, data);
   }
 
-  SerialUSB.println("### Send.");
-  SerialUSB.print("Send:");
-  SerialUSB.print(data);
-  SerialUSB.println("");
-  if (!Wio.SocketSend(connectId, data)) {
-    SerialUSB.println("### ERROR! ###");
-    goto err_close;
+  unsigned long next = millis() + INTERVAL;
+  while (millis() < next) {
+    MqttClient.loop();
   }
-
-  SerialUSB.println("### Receive.");
-  int length;
-  length = Wio.SocketReceive(connectId, data, sizeof (data), RECEIVE_TIMEOUT);
-  if (length < 0) {
-    SerialUSB.println("### ERROR! ###");
-    goto err_close;
-  }
-  if (length == 0) {
-    SerialUSB.println("### RECEIVE TIMEOUT! ###");
-    goto err_close;
-  }
-  SerialUSB.print("Receive:");
-  SerialUSB.print(data);
-  SerialUSB.println("");
-
-err_close:
-  SerialUSB.println("### Close.");
-  if (!Wio.SocketClose(connectId)) {
-    SerialUSB.println("### ERROR! ###");
-    goto err;
-  }
-
-err:
-  delay(INTERVAL);
 }
-
-////////////////////////////////////////////////////////////////////////////////////////
